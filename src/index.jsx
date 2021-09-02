4e90e76f9120453e5a5f88cd7f4b1ea582ef3cd5
David VanDusen <github@dvw3.com>
2021-09-02 15:00:18 -0700

    Add production build script

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,7 @@
-import 'preact/devtools';
+if (process.env.NODE_ENV === 'development') {
+  require('preact/devtools');
+}
+
 import { h, render } from 'preact';
 import App from './components/App.jsx';
 
